1cbf435b18462c19a204845f747226d99602064f
Kylian2 <kylian.richard@gmail.com>
2025-03-22 23:34:12 +0100

    refactor: gestion de l'affichage via le UI Handler

--- Assets/Scripts/Controllers/MainMenuController.cs
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -10,11 +10,25 @@ public class MainMenuController : MonoBehaviour
     void Start()
     {
         startButton = GetComponent<Button>();
-        startButton.onClick.AddListener(startButtonListener);
+        startButton.onClick.AddListener(() => StartButtonListener());
     }
 
-    void startButtonListener()
+    public async void StartButtonListener()
     {
-        SceneManager.LoadScene("LevelSelect");
+        // Commencer le chargement de la scène
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelSelect");
+        
+        // Empêcher la transition automatique vers la nouvelle scène lorsqu'elle est prête
+        asyncLoad.allowSceneActivation = true;
+        
+        // Attendre que la scène soit complètement chargée
+        while (!asyncLoad.isDone)
+        {   
+            // Attendre le prochain frame
+            await System.Threading.Tasks.Task.Yield();
+        }
+        
+        // Maintenant que la scène est chargée, afficher les niveaux
+        UIManager.instance.displayLevels();
     }
 }
--- Assets/Scripts/Core/UIManager.cs
+++ Assets/Scripts/Core/UIManager.cs
@@ -1,4 +1,7 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -7,4 +10,43 @@ public class UIManager : MonoBehaviour
     // - L'écran de défaite
     // - Les pops-up qui peuvent intervenir
     //IL gèrera probablement l'affichage des chronomètres et des scores.
+
+    public static UIManager instance; // Instance unique de UIManager
+    public Button levelButton;
+    public Transform buttonContainer;
+
+    void Awake(){
+    
+        if (instance == null)
+        {
+            instance = this;          
+            DontDestroyOnLoad(this);         
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void displayLevels(){
+        Debug.Log(SceneManager.GetActiveScene().name);
+        if(SceneManager.GetActiveScene().name != "LevelSelect"){
+            return;
+        }
+        Debug.Log("Display levels");
+        buttonContainer = FindObjectOfType<GridLayoutGroup>().transform;
+        for(int i = 0; i < GameManager.NB_LEVELS; i++)
+        {
+            Button button = Instantiate(levelButton, buttonContainer);
+            LevelSelectorController levelSelectorController = FindObjectOfType<LevelSelectorController>();
+            button.onClick.AddListener(delegate { levelSelectorController.handleClick(i+1); });
+            if(GameManager.instance.levelIsLocked(i+1))
+            {
+                button.interactable = false;
+                button.GetComponent<Image>().sprite = Resources.Load<Sprite>("Graphics/Buttons/Level" + (i+1)+"/locked");
+            }else{
+                button.GetComponent<Image>().sprite = Resources.Load<Sprite>("Graphics/Buttons/Level" + (i+1)+"/unlocked");
+            }
+        }
+    }
 }
