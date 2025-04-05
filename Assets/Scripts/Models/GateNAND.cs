b05ac08c2b43bc77c2c7f701e1f56f80dc9cb284
Kylian2 <kylian.richard@gmail.com>
2025-04-05 12:23:47 +0200

    fix: modification du namespace

--- Assets/Scripts/Models/GateNAND.cs
+++ Assets/Scripts/Models/GateNAND.cs
@@ -1,13 +1,13 @@
 using System;
 using LogiSpark.Models;
 
-namespace Models
+namespace LogiSpark.Models
 {
     public class GateNAND : LogicGate
     {
         public override bool Evaluate(bool? input1, bool? input2)
         {
-            return !(input1 && input2);
+            return !(input1.GetValueOrDefault() && input2.GetValueOrDefault());
         }
     }
 }
--- Assets/Scripts/Models/GateNOT.cs
+++ Assets/Scripts/Models/GateNOT.cs
@@ -1,7 +1,7 @@
 using System;
 using LogiSpark.Models;
 
-namespace Models
+namespace LogiSpark.Models
 {
     public class GateNOT : LogicGate
     {
